94e3c4050a467fd2e63fb359689cd32b523dd3be
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2023-10-04 18:05:07 +0300

    initialize oath

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 from website import create_app
+from website import oauth
 
 
 app = create_app()
 
+oauth.init_app(app)
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=5000)
