0c1b1e86e8a38f4a8cbd39a322c5de7460bac43a
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2023-10-04 18:04:48 +0300

    Add google oath config

--- website/__init__.py
+++ website/__init__.py
@@ -1,16 +1,35 @@
 from flask import Flask
+from authlib.integrations.flask_client import OAuth
 from flask_sqlalchemy import SQLAlchemy
 from os import path
 from flask_login import LoginManager
 
 db = SQLAlchemy()
-DB_NAME = "database.db"
+
+app_config = {
+    "OAUTH2_CLIENT_ID": "get client id from google console",
+    "OAUTH2_CLIENT_SECRET": "get client secret from google console",
+    "OAUTH2_META_URL": "https://accounts.google.com/.well-known/openid-configuration",
+    "FLASK_SECRET": "my super secure sexy secret key",
+    "FLASK_PORT": 5000,
+    "DB_NAME": "database.db"
+}
+
+oauth = OAuth()
+
+oauth.register("notes_app",
+               client_id=app_config.get("OAUTH2_CLIENT_ID"),
+               client_secret=app_config.get("OAUTH2_CLIENT_SECRET"),
+               server_metadata_url=app_config.get("OAUTH2_META_URL"),
+               client_kwargs={
+                   "scope": "openid profile email https://www.googleapis.com/auth/user.birthday.read https://www.googleapis.com/auth/user.gender.read"
+               })
 
 
 def create_app():
     app = Flask(__name__)
-    app.config["SECRET_KEY"] = "my super secure sexy secret key"
-    app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{DB_NAME}"
+    app.config["SECRET_KEY"] = app_config.get("FLASK_SECRET")
+    app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{app_config.get('DB_NAME')}"
 
     db.init_app(app)
 
@@ -36,7 +55,7 @@ def create_app():
 
 
 def create_database(app):
-    if not path.exists("website/" + DB_NAME):
+    if not path.exists("website/" + app_config.get('DB_NAME')):
         with app.app_context():
             db.create_all()
             print("Created Database!")
