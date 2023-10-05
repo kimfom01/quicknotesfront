be75514eafa5970e02279091726dbe08a2a745d9
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2023-10-06 00:12:36 +0300

    login user upon successful registration

--- website/auth.py
+++ website/auth.py
@@ -72,6 +72,7 @@ def sign_up():
             db.session.add(new_user)
             db.session.commit()
             flash("Account created!", category="success")
+            login_user(new_user)
             return redirect(url_for("views.home"))
     return render_template("sign_up.html", user=current_user)
 
