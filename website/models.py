69a76eecd14ae6bcea34c0797a48c7060eeed4bc
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2023-10-12 23:00:19 +0300

    Add notes collection model

--- website/models.py
+++ website/models.py
@@ -3,16 +3,24 @@ from flask_login import UserMixin
 from sqlalchemy.sql import func
 
 
-class Note(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    data = db.Column(db.String(10000))
-    date = db.Column(db.DateTime(timezone=True), default=func.now())
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-
-
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(150), unique=True)
     password = db.Column(db.String(150))
     first_name = db.Column(db.String(150))
+    collections = db.relationship('Collection')
+
+
+class Collection(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(150), unique=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     notes = db.relationship('Note')
+
+
+class Note(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    data = db.Column(db.String(10000))
+    date = db.Column(db.DateTime(timezone=True), default=func.now())
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    collection_id = db.Column(db.Integer, db.ForeignKey('collection.id'))
