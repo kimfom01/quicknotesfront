1a1f8b96c059abd3919789ae760e000b790e07e0
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2023-12-06 07:58:46 +0300

    Check for 204 no content status, otherwise throw exception

--- website/static/js/index.js
+++ website/static/js/index.js
@@ -1,10 +1,12 @@
 deleteNote = (noteId) => {
   try {
     fetch("/delete-note", {
-      method: "POST",
+      method: "DELETE",
       body: JSON.stringify({ noteId }),
     }).then((result) => {
-      console.log(result);
+      if (result.status !== 204) {
+        throw new Error("Unable to delete");
+      }
       window.location.reload();
     });
   } catch (err) {
