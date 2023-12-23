bdecf57dbab99546f691e6fa4dcf9186a8657f4a
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2023-12-23 15:43:08 +0300

    Update import

--- back/website/__init__.py
+++ back/website/__init__.py
@@ -47,7 +47,7 @@ def create_app():
     app.register_blueprint(auth, url_prefix="/")
     app.register_blueprint(notes, url_prefix="/")
 
-    from .models import User
+    from .models.User import User
 
     Migrate(app, db)
 
