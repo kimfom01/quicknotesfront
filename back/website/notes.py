f61fc6f013283ad7f647a0613dc37fbdb2423bb7
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2023-12-23 16:25:38 +0300

    Refactor notes route to work with notes repo and collection

--- back/website/notes.py
+++ back/website/notes.py
@@ -1,8 +1,11 @@
 import json
-from flask import Blueprint, request, render_template, jsonify, flash
+from flask import Blueprint, request, render_template, jsonify, flash, redirect, url_for
 from flask_login import login_required, current_user
-from .models import Note, Collection
-from . import db
+
+from .models.Collection import Collection
+
+from .models.Note import Note
+from .repositories.notes_repo import notes_repo
 
 
 notes = Blueprint("notes", __name__)
@@ -17,9 +20,9 @@ def my_notes():
 
     collection_id = request.args.get("collection_id")
 
-    collection = Collection.query.get(collection_id)
+    response = notes_repo.get_all(collection_id)
 
-    return render_template("my_notes.html", user=current_user, collection=collection)
+    return render_template("my_notes.html", user=current_user, notes=response.body)
 
 
 @notes.route("/my-collections", methods=["GET"])
@@ -28,12 +31,11 @@ def my_collections():
     """
     Show collections
     """
-    collection_id = request.args.get("collection_id")
 
-    collection = Collection.query.get(collection_id)
+    response = notes_repo.get_collections(user_id=current_user.id)
 
     return render_template(
-        "my_collections.html", user=current_user, collection=collection
+        "my_collections.html", user=current_user, collection=response.body
     )
 
 
@@ -43,38 +45,51 @@ def new_note():
     """
     Create new note
     """
-    collection_id = Collection.query.filter_by(user_id=current_user.id).first().id
-    if request.method == "POST":
-        note = request.form.get("note")
 
-        if len(note) < 1:
+    collections = Collection.query.filter_by(user_id=current_user.id).all()
+
+    if request.method == "POST":
+        data = request.form.get("note")
+        collection_id: int = request.form.get("collection_id")
+
+        if len(data) < 1:
             flash("Note is too short", category="error")
-        else:
-            created_note = Note(
-                data=note, user_id=current_user.id, collection_id=collection_id
+            return render_template(
+                "new_note.html", user=current_user, collections=collections
             )
 
-            db.session.add(created_note)
-            db.session.commit()
+        created_note = Note(
+            data=data, user_id=current_user.id, collection_id=collection_id
+        )
+
+        response = notes_repo.create_note(note=created_note)
+
+        if response.success:
             flash("Note saved!", category="success")
-    return render_template("new_note.html", user=current_user)
+            return redirect(url_for("notes.my_notes", collection_id=collection_id))
+
+        flash(response.message, category="error")
+        return render_template(
+            "new_note.html", user=current_user, collections=collections
+        )
+    return render_template("new_note.html", user=current_user, collections=collections)
 
 
 @notes.route("/delete-note", methods=["DELETE"])
 @login_required
 def delete_note():
     """
-    Delete existing note api endpoint
+    Delete existing note endpoint
     """
     data = json.loads(request.data)
     note_id = data["noteId"]
+    collection_id = data["collectionId"]
 
-    note = Note.query.get(note_id)
+    response = notes_repo.delete_note(note_id=note_id, collection_id=collection_id)
 
-    if not note:
-        return jsonify({}), 404
+    # TODO: Fix 500 error return status
 
-    if note.user_id == current_user.id:
-        db.session.delete(note)
-        db.session.commit()
-    return jsonify({}), 204
+    if response.success:
+        return jsonify({"message": response.message}), 204
+
+    return jsonify({"message": response.message}), 404
