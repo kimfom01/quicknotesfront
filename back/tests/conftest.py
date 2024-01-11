303f8621970d2ee6ec7e2169928ea0d0b4d767e9
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2024-01-11 10:34:21 +0300

    wire up a logged in client

--- back/tests/conftest.py
+++ back/tests/conftest.py
@@ -9,3 +9,13 @@ def client():
 
     with app.app_context(), app.test_client() as client:
         yield client
+
+
+@pytest.fixture
+def client_logged_in():
+    app = create_app()
+
+    with app.app_context(), app.test_client() as client:
+        # Login demo user
+        client.get("/demo-login")
+        yield client
