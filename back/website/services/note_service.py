b37469d7aa0c5ef26195bcbfedb78f5a6dda10f8
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2024-01-16 03:17:21 +0300

    Move logic to service layer

---
+++ back/website/services/note_service.py
@@ -0,0 +1,46 @@
+from website.repositories import notes_repo
+from website.schema.Response import Response
+from website.repositories.notes_repo import NotesRepo, notes_repo
+
+
+class NoteService:
+    def __init__(self, notes_repo: NotesRepo) -> None:
+        self.notes_repo = notes_repo
+
+    def get_by_id(self, id: int, collection_id: int) -> Response:
+        note = self.notes_repo.get_by_id(id=id, collection_id=collection_id)
+
+        if note is None:
+            return Response(success=False, message="Note not found", body=None)
+        return Response(success=True, message="Success", body=note)
+
+    def get_all(self, collection_id: int) -> Response:
+        notes = self.notes_repo.get_all(collection_id=collection_id)
+
+        if notes is None:
+            return Response(success=False, message="Notes not found", body=None)
+        return Response(success=True, message="Success", body=notes)
+
+    def create_note(self, data: str, user_id: int, collection_id: int) -> Response:
+        try:
+            if len(data) < 1:
+                return Response(success=False, message="Unable to create", body=None)
+
+            note = self.notes_repo.create_note(
+                data=data, user_id=user_id, collection_id=collection_id
+            )
+
+            return Response(success=True, message="Successfully created", body=note)
+        except:
+            return Response(success=False, message="Unable to create", body=None)
+
+    def delete_note(self, note_id: int, collection_id: int) -> Response:
+        try:
+            self.notes_repo.delete_note(note_id=note_id, collection_id=collection_id)
+
+            return Response(success=True, message="Successfully deleted", body=None)
+        except Exception as ex:
+            return Response(success=False, message=str(ex), body=None)
+
+
+note_service = NoteService(notes_repo)
