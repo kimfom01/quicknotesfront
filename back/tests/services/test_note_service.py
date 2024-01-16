574d4e2bac1fbf10dc9c37ee33eb7f4455262416
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2024-01-16 03:17:39 +0300

    Add unit tests for service layer

---
+++ back/tests/services/test_note_service.py
@@ -0,0 +1,91 @@
+from website.schema.Response import Response
+from website.services.note_service import NoteService
+from website.repositories.notes_repo import NotesRepo
+import pytest
+from unittest import mock
+
+
+@pytest.fixture
+def note_repo():
+    mock_note_repo = mock.create_autospec(NotesRepo)
+
+    yield mock_note_repo
+
+
+def test_get_by_id_valid(note_repo):
+    note_service = NoteService(note_repo)
+
+    response = note_service.get_by_id(id=1, collection_id=1)
+
+    assert response.success == True
+    assert response.message == "Success"
+
+
+def test_get_by_id_invalid(note_repo):
+    note_repo.get_by_id.return_value = None
+    note_service = NoteService(note_repo)
+
+    response = note_service.get_by_id(id=1, collection_id=0)
+
+    assert response.success == False
+    assert response.message == "Note not found"
+
+
+def test_get_all_valid(note_repo):
+    note_service = NoteService(note_repo)
+
+    response = note_service.get_all(collection_id=1)
+
+    assert response.success == True
+    assert response.message == "Success"
+
+
+def test_get_all_invalid(note_repo):
+    note_repo.get_all.return_value = None
+    note_service = NoteService(note_repo)
+
+    response = note_service.get_all(collection_id=0)
+
+    assert response.success == False
+    assert response.message == "Notes not found"
+
+
+def test_create_note_valid(note_repo):
+    note_service = NoteService(note_repo)
+
+    response = note_service.create_note(data="test data", user_id=1, collection_id=1)
+
+    assert response.success == True
+    assert response.message == "Successfully created"
+
+
+def test_create_note_invalid(note_repo):
+    note_repo.create_note.return_value = Response(
+        success=False, message="Unable to create", body=None
+    )
+    note_service = NoteService(note_repo)
+
+    response = note_service.create_note(data="", user_id=1, collection_id=1)
+
+    assert response.success == False
+    assert response.message == "Unable to create"
+
+
+def test_delete_note(note_repo):
+    note_service = NoteService(note_repo)
+
+    response = note_service.delete_note(note_id=1, collection_id=1)
+
+    assert response.success == True
+    assert response.message == "Successfully deleted"
+
+
+def test_delete_note_invalid(note_repo):
+    note_repo.delete_note.side_effect = Exception("Unable to delete")
+
+    note_service = NoteService(note_repo)
+
+    response = note_service.delete_note(note_id=1, collection_id=0)
+
+    assert response.success == False
+    assert response.message == "Unable to delete"
