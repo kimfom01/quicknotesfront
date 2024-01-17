f04b8b11edef5d6fb9505948e88766a599eae59b
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2024-01-17 16:23:26 +0300

    Create end point route for update note along with page

--- back/website/routers/notes_route.py
+++ back/website/routers/notes_route.py
@@ -20,7 +20,9 @@ def my_notes():
     collection_id = request.args.get("collection_id")
 
     try:
-        response = note_service.get_all(int(collection_id))
+        response = note_service.get_all(
+            user_id=current_user.id, collection_id=int(collection_id)
+        )
     except:
         return render_template("404.html", user=current_user)
 
@@ -50,7 +52,7 @@ def new_note():
 
     response = collection_repo.get_collections(user_id=current_user.id)
     if not response.success:
-        flash("Something went wrong, try again", category="error")
+        "Something went wrong, unable to get user's collections",
         return
 
     collections = response.body
@@ -64,7 +66,7 @@ def new_note():
         )
 
         if response.success:
-            flash("Note saved!", category="success")
+            flash(response.message, category="success")
             return redirect(url_for("notes.my_notes", collection_id=collection_id))
 
         flash(response.message, category="error")
@@ -74,15 +76,71 @@ def new_note():
     return render_template("new_note.html", user=current_user, collections=collections)
 
 
+@notes.route("/update-note", methods=["GET", "POST"])
+@login_required
+def update_note():
+    """
+    Update existing note
+    """
+    if request.method == "GET":
+        note_id = request.args.get("note_id")
+        collection_id = request.args.get("collection_id")
+
+        response = collection_repo.get_collections(user_id=current_user.id)
+        if not response.success:
+            flash(
+                "Something went wrong, unable to get user's collections",
+                category="error",
+            )
+        else:
+            collections = response.body
+
+        response = note_service.get_by_id(
+            id=int(note_id), user_id=current_user.id, collection_id=int(collection_id)
+        )
+        if response.success:
+            return render_template(
+                "update_note.html",
+                note=response.body,
+                user=current_user,
+                collections=collections,
+            )
+        else:
+            flash(response.message, category="error")
+            return render_template("404.html", user=current_user)
+    else:
+        note_id = request.form.get("note_id")
+        collection_id = request.form.get("collection_id")
+        data: str = request.form.get("note")
+
+        response = note_service.update_note(
+            data=data,
+            note_id=int(note_id),
+            user_id=current_user.id,
+            collection_id=int(collection_id),
+        )
+
+        if response.success:
+            flash(response.message, category="success")
+            return redirect(url_for("notes.my_notes", collection_id=collection_id))
+        else:
+            flash(response.message, category="error")
+            return redirect(
+                url_for(
+                    "notes.update_note", collection_id=collection_id, note_id=note_id
+                )
+            )
+
+
 @notes.route("/delete-note", methods=["DELETE"])
 @login_required
 def delete_note():
     """
     Delete existing note endpoint
     """
-    data = json.loads(request.data)
-    note_id = data["noteId"]
-    collection_id = data["collectionId"]
+    request_data = json.loads(request.data)
+    note_id = request_data["noteId"]
+    collection_id = request_data["collectionId"]
 
     response = note_service.delete_note(
         note_id=note_id, user_id=current_user.id, collection_id=collection_id
