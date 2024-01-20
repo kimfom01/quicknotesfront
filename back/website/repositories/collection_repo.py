dd172befcd090cb1e32bb9f946e2ea5fe47c8ba2
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2024-01-20 15:58:36 +0300

    Move collection logic to service layer

--- back/website/repositories/collection_repo.py
+++ back/website/repositories/collection_repo.py
@@ -1,3 +1,4 @@
+from typing import List
 from ..models.Collection import Collection
 from ..schema.Response import Response
 from .. import db
@@ -7,37 +8,31 @@ DEFAULT_COLLECTION = "Default Collection"
 
 
 class CollectionRepo:
-    def get_collections(self, user_id: int) -> Response:
-        collections = Collection.query.filter_by(user_id=user_id).all()
+    def get_collections(self, user_id: int) -> List[Collection]:
+        return Collection.query.filter_by(user_id=user_id).all()
 
-        if collections is None:
-            return Response(success=False, message="Collections not found", body=None)
-        return Response(success=True, message="Success", body=collections)
-
-    def get_default_collection(self, user_id: int) -> Response:
-        collection = Collection.query.filter_by(
+    def get_default_collection(self, user_id: int) -> Collection:
+        return Collection.query.filter_by(
             user_id=user_id, title=DEFAULT_COLLECTION
         ).first()
 
-        if collection is None:
-            return Response(
-                success=False, message="Default Collection not found", body=None
-            )
-        return Response(success=True, message="Success", body=collection)
+    def create_collection(self, title: str, user_id: int) -> Collection:
+        collection = Collection(title=title, user_id=user_id)
 
-    def create_collection(self, title: str, user_id: int) -> Response:
-        try:
-            collection = Collection(title=title, user_id=user_id)
+        db.session.add(collection)
+        db.session.commit()
+        db.session.refresh(collection)
 
-            db.session.add(collection)
-            db.session.commit()
-            db.session.refresh(collection)
+        return collection
 
-            return Response(
-                success=True, message="Successfully created", body=collection
-            )
-        except:
-            return Response(success=False, message="Unable to create", body=None)
+    def create_default_collection(self, user_id: int) -> Collection:
+        collection = Collection(title=DEFAULT_COLLECTION, user_id=user_id)
+
+        db.session.add(collection)
+        db.session.commit()
+        db.session.refresh(collection)
+
+        return collection
 
 
 collection_repo = CollectionRepo()
