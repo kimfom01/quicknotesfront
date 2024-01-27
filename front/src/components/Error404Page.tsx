6505136b5d9a81a882c556e25a578c67cdd42c46
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2024-01-28 02:22:27 +0300

    Fix error 404 page

--- front/src/components/Error404Page.tsx
+++ front/src/components/Error404Page.tsx
@@ -4,8 +4,8 @@ import "./Error404Page.css";
 export const Error404Page = () => {
   return (
     <div className="n-f-style">
-      <h1 style={{ fontSize: "5rem" }}>404 Page Not Found</h1>
-      <h1>Oops! You seem to be lost.</h1>
+      <h1>404 Page Not Found</h1>
+      <h2>Oops! You seem to be lost.</h2>
       <p>
         Go to:{" "}
         <Link className="text-decoration-none" to="/">
