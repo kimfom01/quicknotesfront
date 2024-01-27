74a6350cce665da0463d6e2dd69afd2658def391
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2024-01-28 02:14:30 +0300

    Create theme layout style

--- front/src/components/Base.tsx
+++ front/src/components/Base.tsx
@@ -1,5 +1,6 @@
 import { ReactElement, ReactNode } from "react";
 import "./Base.css";
+import { Container, Nav, Navbar } from "react-bootstrap";
 
 interface Prop {
   children: ReactNode | ReactElement;
@@ -8,10 +9,37 @@ interface Prop {
 export const Base = ({ children }: Prop) => {
   return (
     <main className="main-bg-render">
-      <div className="container-fluid mt-3">
-        <h1>Quick Notes</h1>
+      <Container fluid className="mt-3">
+        <Navbar collapseOnSelect expand="lg" variant="dark">
+          <Navbar.Brand href="/" className="text-light fw-bold">
+            Quick Notes
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="ms-auto">
+              <span>
+                <Nav.Link className="text-orange" href="/">
+                  Home
+                </Nav.Link>
+                <hr />
+              </span>
+              <Nav.Link className="text-light" href="/about-us">
+                About Us
+              </Nav.Link>
+              <Nav.Link className="text-light" href="/how-it-works">
+                How it works
+              </Nav.Link>
+              <Nav.Link className="text-light" href="/signin">
+                Sign In
+              </Nav.Link>
+              <Nav.Link className="text-light" href="/signup">
+                Sign Up
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
         <div>{children}</div>
-      </div>
+      </Container>
     </main>
   );
 };
