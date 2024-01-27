0d13c0eb39504a652ad3d14abe230f0a1c413595
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2024-01-28 01:08:49 +0300

    Setup browser router

--- front/src/index.tsx
+++ front/src/index.tsx
@@ -1,15 +1,44 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import "bootstrap/dist/css/bootstrap.min.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import reportWebVitals from "./reportWebVitals";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { LandingPage } from "./components/LandingPage";
+import { Error404Page } from "./components/Error404Page";
+import { Login } from "./components/Login";
+import { SignUp } from "./components/SignUp";
+import { NotesPage } from "./components/NotesPage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <LandingPage />,
+    errorElement: <Error404Page />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+    errorElement: <Error404Page />,
+  },
+  {
+    path: "/signup",
+    element: <SignUp />,
+    errorElement: <Error404Page />,
+  },
+  {
+    path: "/notes",
+    element: <NotesPage />,
+    errorElement: <Error404Page />,
+  },
+  { path: "*", element: <Error404Page /> },
+]);
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
